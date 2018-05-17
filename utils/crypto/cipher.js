77c63bd14421212dafd8ee8cd25196fe280f735c
Zillion <skyestzhang@163.com>
2018-05-17 20:26:18 +0800

    bug

--- utils/crypto/cipher.js
+++ utils/crypto/cipher.js
@@ -1,9 +1,10 @@
 const crypto = require('crypto');
-const cipher = crypto.createCipher('aes192', '20d6743b93893568a964755201e25c8a');
-const decipher = crypto.createDecipher('aes192', '20d6743b93893568a964755201e25c8a');
 
 
 exports.encrypted=function (origin_pwd) {
+    const cipher = crypto.createCipher('aes192', '20d6743b93893568a964755201e25c8a');
+
+    console.log(origin_pwd);
     let encrypted = cipher.update(origin_pwd, 'utf8', 'hex');
     encrypted += cipher.final('hex');
    // console.log(encrypted);
@@ -13,6 +14,7 @@ exports.encrypted=function (origin_pwd) {
 
 
 exports.decrypted=function (Ciphertext) {
+    const decipher = crypto.createDecipher('aes192', '20d6743b93893568a964755201e25c8a');
 
     let encrypted =Ciphertext;
     let decrypted = decipher.update(encrypted, 'hex', 'utf8');
